a785e314362e104967493633b53c9ccb0ce1715e
Linda Do <lindado_98@hotmail.com>
2025-08-20 12:48:09 +0200

    author ok

--- src/main/java/com/booleanuk/api/controllers/AuthorController.java
+++ src/main/java/com/booleanuk/api/controllers/AuthorController.java
@@ -4,10 +4,10 @@ package com.booleanuk.api.controllers;
 import com.booleanuk.api.models.Author;
 import com.booleanuk.api.repositories.AuthorRepsitory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,4 +22,41 @@ public class AuthorController {
     public ResponseEntity<List<Author>> getAllAuthors() {
         return ResponseEntity.ok(this.authorRepsitory.findAll());
     }
+
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        return new ResponseEntity<>(this.authorRepsitory.save(author), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable int id) {
+        Author author = this.authorRepsitory.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No authors with that id were found"));
+        return ResponseEntity.ok(author);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Author> updateAuthor(@PathVariable int id, @RequestBody Author author) {
+        Author authorToUpdate = this.authorRepsitory.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No authors with that id were found to update"));
+
+        authorToUpdate.setFirstName(author.getFirstName());
+        authorToUpdate.setLastName(author.getLastName());
+        authorToUpdate.setEmail(author.getEmail());
+        authorToUpdate.setAlive(author.isAlive());
+        authorToUpdate.setBooks(author.getBooks());
+
+        return new ResponseEntity<>(this.authorRepsitory.save(authorToUpdate), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Author> deleteAuthor(@PathVariable int id) {
+        Author authorToDelete = this.authorRepsitory.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No authors with that id were found to delete"));
+
+        this.authorRepsitory.delete(authorToDelete);
+        return ResponseEntity.ok(authorToDelete);
+    }
+
+
 }
